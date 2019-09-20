e4d17e8154bcb09052b0da3033d6b9f3f6b79304
Nerrosh <Nerrosh@DESKTOP-3GFFVUE>
2019-09-19 22:49:15 -0300

    Build 19/09/2019

---
+++ src/ffmpeg/ui/RT.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ffmpeg.ui;
+
+/**
+ *
+ * @author Nerrosh
+ */
+import java.io.*;
+class RT extends Thread
+{
+    InputStream is;
+    String type;
+    
+    RT(InputStream is, String type)
+    {
+        this.is = is;
+        this.type = type;
+    }
+    
+    public void run()
+    {
+        try
+        {
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line=null;
+            while ( (line = br.readLine()) != null)
+                System.out.println(type + ">" + line);    
+            } catch (IOException ioe)
+              {
+                ioe.printStackTrace();  
+              }
+    }
+}
